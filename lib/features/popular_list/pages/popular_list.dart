d39176200693ca0ec090bb8c1d8631ec14758a1e
HienBull98 <phunghien9812@gmail.com>
2023-08-30 00:12:41 +0700

    add models and update gridview

--- lib/features/popular_list/pages/popular_list.dart
+++ lib/features/popular_list/pages/popular_list.dart
@@ -9,6 +9,7 @@ class PopularListPage extends StatefulWidget {
 }
 
 class _PopularListPageState extends State<PopularListPage> {
+  final List<int> numbers = List.generate(100, (index) => index + 1);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,18 +28,41 @@ class _PopularListPageState extends State<PopularListPage> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Container(
-        color: Colors.white,
-        width: double.maxFinite,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("Popular list"),
-            SizedBox(
-              height: 10.h,
-            ),
-            _itemGridView(context),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.w),
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Popular list",
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                    ),
+                    itemCount: 18,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return _itemGridView(context);
+                    }),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -46,143 +70,102 @@ class _PopularListPageState extends State<PopularListPage> {
 
   Widget _itemGridView(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10.w),
-      width: 150.w,
-      height: 200.h,
-      decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(40.0))),
-      child: Stack(
-        children: [
-          Image.network(
-            "https://thesaigontimes.vn/Uploads/Articles/67617/6177b_mission_impossible_4___int_poster.jpg",
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Stack(
-              children: [
-                Positioned(
-                    top: 0,
-                    right: 0,
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Colors.orange,
-                                Colors.pinkAccent,
-                              ],
-                            )),
-                        child: Center(
-                          child: RichText(
-                            text: const TextSpan(
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.white),
-                              children: [
-                                TextSpan(
-                                  text: '9',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                TextSpan(
-                                  text: '.8',
-                                ),
-                              ],
+      height: 350.h,
+      width: 130.w,
+      // decoration: const BoxDecoration(
+      //   borderRadius: BorderRadius.all(Radius.circular(20)),
+      // ),
+      decoration: BoxDecoration(
+          color: Colors.redAccent,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.greenAccent,
+                blurRadius: 5.0,
+                blurStyle: BlurStyle.outer)
+          ]),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Stack(
+          children: [
+            Image.network(
+              "https://thesaigontimes.vn/Uploads/Articles/67617/6177b_mission_impossible_4___int_poster.jpg",
+              width: double.maxFinite,
+              height: 350.h,
+              fit: BoxFit.fill,
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 0,
+                      right: 0,
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Colors.orange,
+                                  Colors.pinkAccent,
+                                ],
+                              )),
+                          child: Center(
+                            child: RichText(
+                              text: const TextSpan(
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.white),
+                                children: [
+                                  TextSpan(
+                                    text: '9',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  TextSpan(
+                                    text: '.8',
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    )),
-                const Positioned(
-                    left: 0,
-                    bottom: 0,
-                    child: Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "2008",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Text(
-                            "Nhiệm vụ bất khả thi",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                      )),
+                  const Positioned(
+                      left: 0,
+                      bottom: 0,
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "2008",
+                              style: TextStyle(color: Colors.white),
                             ),
-                          ),
-                        ],
-                      ),
-                    ))
-              ],
+                            Text(
+                              "Nhiệm vụ bất khả thi",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
             ),
-          ),
-          // Container(
-          //   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          //   height: double.maxFinite,
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: [
-          //       Container(
-          //         width: 30,
-          //         height: 30,
-          //         decoration: const BoxDecoration(
-          //             shape: BoxShape.circle,
-          //             gradient: LinearGradient(
-          //               begin: Alignment.topLeft,
-          //               end: Alignment.bottomRight,
-          //               colors: [
-          //                 Colors.orange,
-          //                 Colors.pinkAccent,
-          //               ],
-          //             )),
-          //         child: Center(
-          //           child: RichText(
-          //             text: const TextSpan(
-          //               style: TextStyle(fontSize: 12, color: Colors.white),
-          //               children: [
-          //                 TextSpan(
-          //                   text: '9',
-          //                   style: TextStyle(
-          //                       fontWeight: FontWeight.bold, fontSize: 16),
-          //                 ),
-          //                 TextSpan(
-          //                   text: '.8',
-          //                 ),
-          //               ],
-          //             ),
-          //           ),
-          //         ),
-          //       ),
-          //       Column(
-          //         crossAxisAlignment: CrossAxisAlignment.start,
-          //         children: [
-          //           Text(
-          //             "2008",
-          //             style: TextStyle(color: Colors.white),
-          //           ),
-          //           Text(
-          //             "Nhiệm vụ bất khả thi",
-          //             style: TextStyle(
-          //               color: Colors.white,
-          //               fontWeight: FontWeight.bold,
-          //             ),
-          //           ),
-          //         ],
-          //       )
-          //     ],
-          //   ),
-          // )
-        ],
+          ],
+        ),
       ),
     );
   }
